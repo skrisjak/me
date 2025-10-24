53b04838ba1485e81f38a512cd9fd50882ef3868
skrisjak <skrisjak@users.noreply.github.com>
2025-10-24 23:09:30 +0000

    deploy: b31be34257d9ec403e706d17a4a3fb5ce951bf0c

---
+++ _next/static/chunks/53eb8042089b060c.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/bac4c1c2c2e01a50.js",
+  "static/chunks/d7b444d246193a2e.js",
+  "static/chunks/a56b5808f9a42f2d.js",
+  "static/chunks/turbopack-883ec17d7ed0296e.js"
+])
---
+++ _next/static/qfph0uQCetHgRUSFe0jV9/_buildManifest.js
@@ -0,0 +1,23 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "./static/chunks/53eb8042089b060c.js"
+  ],
+  "/_error": [
+    "./static/chunks/e4f5bcc5b37a658c.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/me//_next/:path+",
+        "destination": "/me/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
