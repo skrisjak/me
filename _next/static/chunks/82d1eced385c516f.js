8d3fc12bcfa4b9c88d4bd0630a0bd3a1a26c10be
skrisjak <skrisjak@users.noreply.github.com>
2025-11-03 16:02:38 +0000

    deploy: 2909001f0c5eb80d805c64e4e5629f3b68645838

--- _next/static/chunks/82d1eced385c516f.js
+++ _next/static/chunks/82d1eced385c516f.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/302fcbd1cd3f1cee.js",
   "static/chunks/15883eb046096fdb.js",
-  "static/chunks/c0f82c33658c54a7.css",
-  "static/chunks/turbopack-25acbe6b4675bd62.js"
+  "static/chunks/0d0cc4e7bba201ec.css",
+  "static/chunks/turbopack-7771604191074b96.js"
 ])
